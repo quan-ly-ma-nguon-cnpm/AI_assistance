e158b3ec28ebed33bae6a39cf63dd09f9b720c0e
Duc Thuan <ducthuan@gmail.com>
2025-05-26 10:17:29 +0700

    Duc Thuan

--- AI_Application/obj/Debug/net9.0/AI_Application.AssemblyInfo.cs
+++ AI_Application/obj/Debug/net9.0/AI_Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI_Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda284d8f15811cd0285a42d4804a1ef24004150")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cda6d684c453ecdc58aedeaf6600aec38fae21a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI_Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI_Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
