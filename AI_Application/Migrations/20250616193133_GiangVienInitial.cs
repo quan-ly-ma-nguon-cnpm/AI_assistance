e9ab546fb5d8922071abe578cdbdad97b6538c4e
ngocduy <ngocduy250604@gmail.com>
2025-06-17 10:33:10 +0700

    commit

---
+++ AI_Application/Migrations/20250616193133_GiangVienInitial.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AI_Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class GiangVienInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NoiDung",
+                table: "CauHois",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ThoiGianGui",
+                table: "CauHois",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "PhanHois",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NoiDung = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    NguoiGui = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TieuDe = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LinhVuc = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    NgayTao = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ThoiGianGui = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DaDuyet = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhanHois", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PhanHois");
+
+            migrationBuilder.DropColumn(
+                name: "NoiDung",
+                table: "CauHois");
+
+            migrationBuilder.DropColumn(
+                name: "ThoiGianGui",
+                table: "CauHois");
+        }
+    }
+}
